4a3428a3f1ce55bbb56571298000756a2e6c27c9
GorkemKutbay <kutbaygorkem2007@gmail.com>
2026-02-07 19:21:10 +0300

    Fixed Home Controller

--- eTicaretUygulamasi/eTicaretUygulamasi.Mvc/Controllers/HomeController.cs
+++ eTicaretUygulamasi/eTicaretUygulamasi.Mvc/Controllers/HomeController.cs
@@ -6,30 +6,34 @@ namespace eTicaretUygulamasi.Mvc.Controllers
 {
     public class HomeController : Controller
     {
-        static private List<string> _sehirler = new List<string>() { "Ankara", "İstanbul", "İzmir" };
-
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult AboutUs()
         {
             return View();
+
+        }
+        public IActionResult Contact()
+        {
+            return View();
+
+        }
+        public IActionResult Listing()
+        {
+            return View();
+
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        
+        public IActionResult ProductDetail()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
+
         }
-        //github kontrol
+
+
     }
 }
