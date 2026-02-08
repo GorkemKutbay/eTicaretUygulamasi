7ee833d41220a3876e8eede223ac881efaa67972
İnci BATMAN BAŞEL <incibatman@gmail.com>
2026-02-08 13:49:39 +0300

    admin home ve controller

---
+++ eTicaretUygulamasi/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        
+        [HttpGet("/category/create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost("/category/{id}/edit")]
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+
+
+        // Delete
+        [HttpGet("/category/{id}/delete")]
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+        [HttpPost("/category/{id}/delete")]
+        public IActionResult Delete(int id)
+        {
+            return View();
+
+        }
+
+    }
+}
