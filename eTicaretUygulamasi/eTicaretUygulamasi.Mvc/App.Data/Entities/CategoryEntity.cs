13c83cba32de6d7b73c8fab6d69df0dbe68ace2e
İnci BATMAN BAŞEL <incibatman@gmail.com>
2026-02-18 17:23:52 +0300

    CategoryEntity ve CartItemEntity eklendi.

---
+++ eTicaretUygulamasi/eTicaretUygulamasi.Mvc/App.Data/Entities/CategoryEntity.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace eTicaretUygulamasi.Mvc.App.Data.Entities
+{
+    public class CategoryEntity
+    {
+        [Required, Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(100, MinimumLength = 2)]
+        public string Name { get; set; } = null!; 
+
+        [Required, StringLength(6, MinimumLength = 3)]
+        public string Color { get; set; } = null!;
+        [Required, StringLength(50, MinimumLength = 2)]
+        public string IconCssClass { get; set; } = null!;
+        [Required]
+        public DateTime CreatedAt { get; set; } 
+    }
+}
