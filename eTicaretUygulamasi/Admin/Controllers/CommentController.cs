548542c7312be7f639d742927db004abeba63c41
GorkemKutbay <kutbaygorkem2007@gmail.com>
2026-03-08 21:19:55 +0300

    comment , User list and approve action created

--- eTicaretUygulamasi/Admin/Controllers/CommentController.cs
+++ eTicaretUygulamasi/Admin/Controllers/CommentController.cs
@@ -1,16 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eTicaretUygulamasi.Mvc.App.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Admin.Controllers
 {
     public class CommentController : Controller
     {
+        private readonly AppDbContext _dbcontext;
+
+        public CommentController(AppDbContext dbContext)
+        {
+            _dbcontext = dbContext;
+        }
         public IActionResult List()
         {
-            return View();
+            var comments = _dbcontext.ProductComments
+                .Include(x => x.User)
+                .Include(x => x.Product)
+                .ToList();
+
+            return View(comments);
         }
-        public IActionResult Approve()
+        [HttpPost]
+        public IActionResult Approve(int id )
         {
-            return View();
+            var comment = _dbcontext.ProductComments.Find(id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+            comment.IsConfirmed = true;
+            _dbcontext.SaveChangesAsync();
+            return RedirectToAction("List");
         }
     }
 }
--- eTicaretUygulamasi/Admin/Controllers/UserController.cs
+++ eTicaretUygulamasi/Admin/Controllers/UserController.cs
@@ -1,18 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eTicaretUygulamasi.Mvc.App.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Admin.Controllers
 {
     public class UserController : Controller
     {
-       //Listele
-       public IActionResult List()
-       {
-           return View();
-       }
+        private readonly AppDbContext _dbcontext;
 
-       public IActionResult Approve (int id)
-       {
-           return View();
-       }
+        //Listele
+        public UserController(AppDbContext dbContext)
+        {
+            _dbcontext = dbContext;
+        }
+        public IActionResult List()
+        {
+            var users = _dbcontext.Users
+                .Include(x => x.Role)
+                .ToList();
+
+            return View(users);
+        }
+
+        public IActionResult Approve(int id)
+        {
+            var user = _dbcontext.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            user.RoleId = 2; 
+            _dbcontext.SaveChanges();
+            return RedirectToAction("List");
+        }
     }
 }
